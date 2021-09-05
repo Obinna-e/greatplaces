682e00495b4d8ef4c446a960684b23cc0e7a8fd6
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2021-09-05 17:18:14 +0100

    update

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_config/flutter_config.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 final String googleApiKey = FlutterConfig.get('GOOGLE_API_KEY');
 
@@ -8,5 +10,20 @@ class LocationHelper {
     double longitude,
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$googleApiKey';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final params = {
+      'latlng': '$lat,$lng',
+      'key': googleApiKey,
+    };
+    final url = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      params,
+    );
+
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -17,11 +17,31 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                      //returning data back with pop
+                    },
+              icon: Icon(Icons.check),
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -29,6 +49,16 @@ class _MapScreenState extends State<MapScreen> {
               widget.initialLocation.longitude),
           zoom: 16,
         ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation,
+                ),
+              },
+//Marker takes sets. sets are like arrays but each value is unique
       ),
     );
   }
